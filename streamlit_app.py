eead5718ad7f43b4db80caba88ac08cfea240967
Gaby <gabrieladelcristo@gmail.com>
2025-11-16 16:32:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.markdown("""
     <style>
     /* Sidebar container */
     section[data-testid="stSidebar"] {
-        color: #ffffff;
+        color: #D55858;
         text-align: center;
         background-color: #9CE6E6;
         background-image: linear-gradient(120deg, #33CCCC, #2AA7A7);
